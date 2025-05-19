ae757f418de8fc532132c6f6ecc3f9deb9561d9c
Kuroimichan4 <miriamhernav@campus.monlau.com>
2025-05-19 19:17:25 +0200

    Paso por parámetros juego
    Probando el paso por parámetros con juego simple

---
+++ SolYProJuegoHeroVSEnemy/TestGame/HeroEnemyAndFinalEnemy.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestGame
+{
+	TEST_CLASS(TestGame)
+	{
+	public:
+		
+		TEST_METHOD(TestMet)
+		{
+		}
+	};
+}
---
+++ SolYProJuegoHeroVSEnemy/TestGame/pch.cpp
@@ -0,0 +1,6 @@
+// pch.cpp: el archivo de código fuente correspondiente al encabezado precompilado
+#pragma once          // Directiva para evitar incluir el archivo varias veces
+#include <string>
+#include "pch.h"
+
+// Cuando se utilizan encabezados precompilados, se requiere este archivo de código fuente para que la compilación se realice correctamente.
